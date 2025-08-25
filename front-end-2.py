d496880f7f2dc8efe892753e58a99acd01d953d8
Kerry Daniels <88607552+Dev-Labb@users.noreply.github.com>
2025-08-25 00:23:53 -0400

    Changed back to text widget for now
    Will have to change to accomodate file input down the line, but leaving text box for now.

--- front-end-2.py
+++ front-end-2.py
@@ -25,8 +25,7 @@ st.title("🧠Multi-Modal Bedrock Test")
 # along in body.
 # -------------------------------------------------------------------------
 model_choice = st.selectbox("Select a model:", list(MODELS.keys()))
-prompt = st.chat_input("Enter your prompt here:", accept_file=True)
-prompt
+prompt = st.text_area("Please enter your prompt here:")
 #handles submit element and makes sure you inputted a prompt 
 if st.button("Submit to Model"):
     if not prompt.strip():
@@ -111,4 +110,5 @@ if st.button("Fetch RF Data"):
 
         except Exception as e:
             st.error(f"Request failed: {str(e)}")
+
 
