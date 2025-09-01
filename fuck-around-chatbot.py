2371683dfc934f1ec67c49db6d4923c37f9491de
Kerry Daniels <88607552+Dev-Labb@users.noreply.github.com>
2025-09-01 00:30:37 -0400

    Update fuck-around-chatbot.py

--- fuck-around-chatbot.py
+++ fuck-around-chatbot.py
@@ -1,11 +1,12 @@
 import streamlit as st
 
-message = st.chat_message(name="assistant" avatar="👿", width="stretch")
+message = st.chat_message(name="Yo Mama", avatar="👿", width="stretch")
 
 message.write("👿 Waddup Bitch ass mfer!!!" \
 "Welcome to my App where I like to talk shit!" \
 "And you aint gone do shit about it!!! I heard you like diddy parties." \
 "You probably got diddied ya with ya bitch ass...")
+
 
 
 
