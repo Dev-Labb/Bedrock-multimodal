1d0a4dccffb7acf7f0beb83bf9ee8b97e543f307
Kerry Daniels <88607552+Dev-Labb@users.noreply.github.com>
2025-09-02 03:17:12 -0400

    Update newer-dynamic-lambda.py

--- lambda/newer-dynamic-lambda.py
+++ lambda/newer-dynamic-lambda.py
@@ -1,7 +1,7 @@
 import json
 import psycopg2
 import os
-import datetime
+from datetime import datetime
 #Note to self: consider importing boto3.clientErrors or whatever the library is called again so I can catch errors
 #Same for adding logging options for cloudwatch events etc. 
 
